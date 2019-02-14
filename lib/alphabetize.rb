ff017079ff2a9dbd42a53f52ffe3a8a821ffeb45
Ali Madni <ali555m@hotmail.com>
2019-02-14 23:29:03 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,6 @@
 def alphabetize(arr)
   esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  esp_array = esperanto.split
+  esp_array = esperanto.split(//)
 
   esp_array.each do |letter|
     puts(letter)
