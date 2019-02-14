6b8672e83036a0bf34a93e23e819b8748cd3de0c
Ali Madni <ali555m@hotmail.com>
2019-02-14 23:36:55 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,9 +1,10 @@
 def alphabetize(arr)
   esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz "
   esp_array = esperanto.split(//)
+  nums = [10..38]
+  esperanto_hash ={}
+  esp_array.each_with_index do |character, index|
+    esperanto_hash[character] = nums[index]
 
-  esp_array.each do |letter|
-    puts(letter)
-  end
+
 end
-alphabetize([])
